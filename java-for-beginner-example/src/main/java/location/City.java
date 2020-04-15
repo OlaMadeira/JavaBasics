1cf695852b91390e090d8061e46f131eb9d860aa
OlaMadeira <alinarossol"gmailcom>
2020-04-15 19:01:07 +0300

    15.04

---
+++ java-for-beginner-example/src/main/java/location/City.java
@@ -0,0 +1,14 @@
+package location;
+
+public class City {
+    public  String name;
+    public int population;
+    public City(String x, int y){
+        name = x;
+        population = y;
+    }
+}
+
+
+
+
